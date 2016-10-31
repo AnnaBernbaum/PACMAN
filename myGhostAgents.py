b183b06736a41bbad508a3d206b2c7494c310b78
27grace <27grace@gmail.com>
2016-10-31 17:57:10 +0000

    Update myGhostAgents.py

--- myGhostAgents.py
+++ myGhostAgents.py
@@ -1,1 +1,58 @@
+# myGhostAgents.py
 
+from game import Agent
+from game import Actions
+from game import Directions
+import random
+import util
+
+class MyGhostAgent( Agent ):
+  def __init__( self, index ):
+    self.index = index
+    
+  def getAction( self, state ):
+    action_list = state.getLegalActions( self.index )
+    return action_list[0]
+  
+  def __init__( self, index, prob_attack=0.8, prob_scaredFlee=0.8 ):
+    self.index = index
+    self.prob_attack = prob_attack
+    self.prob_scaredFlee = prob_scaredFlee
+  
+  def getDistribution( self, state ):
+    # Read variables from state
+    ghostState = state.getGhostState( self.index )
+    legalActions = state.getLegalActions( self.index )
+    pos = state.getGhostPosition( self.index )
+    isScared = ghostState.scaredTimer > 0
+    
+    speed = 1
+    if isScared: speed = 0.5
+    actionVectors = [Actions.directionToVector( a, speed ) for a in legalActions]
+    newPositions = [( pos[0]+a[0], pos[1]+a[1] ) for a in actionVectors]
+    pacmanPosition = state.getPacmanPosition()
+    # Select best actions given the state
+    distancesToPacman = [manhattanDistance( pos, pacmanPosition ) for pos in newPositions]
+    if isScared:
+      bestScore = max( distancesToPacman )
+      bestProb = self.prob_scaredFlee
+    else:
+      bestScore = min( distancesToPacman )
+      bestProb = self.prob_attack
+    bestActions = [action for action, distance in zip( legalActions, distancesToPacman ) if distance == bestScore]
+    
+    # Construct distribution
+    dist = util.Counter()
+    for a in bestActions: dist[a] = bestProb / len(bestActions)
+    for a in legalActions: dist[a] += ( 1-bestProb ) / len(legalActions)
+    dist.normalize()
+    return dist
+
+  # Mode 1: Chase Pacman
+  def chasePacman:
+  
+  # Mode 2: Protect Big Biscuits
+  def protectBigBiscuits:
+  
+  # Mode 3: Avoid Pacman
+  def avoidPacman: 
